f4852dd8ad43a57fc6ebca4fb75d74a6d64552d2
mel <melissa.sargent@hotmail.co.uk>
2020-09-01 13:50:12 +0100

    instructions added

---
+++ challenge.js
@@ -0,0 +1,9 @@
+//*  Complete this function to subtract one number from another then console log the returned answer//
+
+const subtractNumber = (number1, number2) => {
+
+}
+
+
+//*  Write a function which takes in a user and returns a string 'Hello (User) welcome's//
+
